bf588495e1a93437e9646927f00c316a25a3f3b7
Rafael Muller <rmuller@mac.com>
2026-05-20 23:00:20 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "nxos" {
+  source  = "netascode/nac-nxos/nxos"
+  version = "0.2.0"
+
+  yaml_directories     = ["data", "data/spines/", "data/leafs/"]
+  template_directories = ["templates/"]
+}
