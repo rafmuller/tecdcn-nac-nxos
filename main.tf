0f1bd7c74307154a5cc449d1696c6172845265f8
Rafael Muller <rmuller@mac.com>
2026-05-24 22:06:53 -0400

    template work

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "nxos" {
   source  = "netascode/nac-nxos/nxos"
   version = "0.2.0"
 
-  yaml_directories     = ["data", "data/spines/", "data/leafs/"]
+  # yaml_directories     = ["data", "data/spines/", "data/leafs/", "data/networks/"]
+  yaml_directories     = ["data/"]
   template_directories = ["templates/"]
 }
